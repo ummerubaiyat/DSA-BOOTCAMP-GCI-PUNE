3d08df5f51d34d7136f14da39074ee9698f71196
Umme Rubaiyat Chowdhury <ummerubaiyat@gmail.com>
2022-04-06 12:52:01 +0600

    Pattern1(contest 1)

---
+++ Umme_Rubaiyat/Basics of Programming/Contest_Solutions/Pattern1.cpp
@@ -0,0 +1,27 @@
+ 
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main(int argc, char **argv){
+    int n,i,t,j,k;
+     cin >> t;
+     
+     for(i=0;i<t;i++)
+     { cin >> n;
+         for(j=1;j<=n;j++)
+         {
+             for(k=1;k<=j;k++)
+         {
+             cout<<"*";
+         }
+         cout<<"\n";
+         }
+     }
+    
+    
+
+    //write your code here
+     
+
+    }
+    
