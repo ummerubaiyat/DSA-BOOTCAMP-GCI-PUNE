673defec9ef5bc5b4313914473f0c11621119417
Umme Rubaiyat Chowdhury <ummerubaiyat@gmail.com>
2022-04-16 14:22:53 +0600

    Basics of Programming

--- Umme_Rubaiyat/Basics of Programming/ReverseNumber.cpp
+++ Umme_Rubaiyat/Basics of Programming/ReverseNumber.cpp
@@ -1,6 +1,6 @@
-// 1. You've to count the number of digits in a number.
-// 2. Take as input "n", the number for which the digits has to be counted.
-// 3. Print the digits in that number.
+// 1. You've to display the digits of a number in reverse.
+// 2. Take as input "n", the number for which digits have to be display in reverse.
+// 3. Print the digits of the number line-wise, but in reverse order.
 #include <iostream>
 using namespace std;
 
@@ -11,7 +11,7 @@ int main(){
     //write your code here
     int count=0;
     while(n!=0)
-    {   //int r = n%10;
+    {   int r = n%10;
         n=n/10;
         cout<<r<<endl;
     }
