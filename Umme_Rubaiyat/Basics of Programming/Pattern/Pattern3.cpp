a2d0727a33aa6f1be7c85c25b83e014f522e7068
Umme Rubaiyat Chowdhury <ummerubaiyat@gmail.com>
2022-04-24 19:33:59 +0600

    Pattern 4

--- Umme_Rubaiyat/Basics of Programming/Pattern/Pattern3.cpp
+++ Umme_Rubaiyat/Basics of Programming/Pattern/Pattern3.cpp
@@ -4,28 +4,30 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    int stars =1;
-    int spaces =n-1;
+    int stars =n;
+    int spaces =1;
    //write your code here
    
    for (int i=1; i <=n; i++)
    {
-       
-      for (int j=1; j <=spaces; j++)
+       for (int k=1; k <=stars; k++)
+        { 
+             cout<<"*\t";
+               
+        }
+       cout<<endl;
+       for (int j=1; j <=spaces; j++)
         { 
             cout<<"\t";
             
                 
         }
-        for (int k=1; k <=stars; k++)
-        { 
-            cout<<"*\t";
-                
-        }
+        
+      
        
-         cout<<endl;
-         stars++;
-         spaces--;
+         
+         stars--;
+         spaces++;
 
    }
    
