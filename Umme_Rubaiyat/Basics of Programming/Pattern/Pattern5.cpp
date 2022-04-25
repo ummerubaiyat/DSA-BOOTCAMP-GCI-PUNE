ea96b3aaa3c52a92f58299db89742f60ce860ed7
Umme Rubaiyat Chowdhury <ummerubaiyat@gmail.com>
2022-04-26 00:04:17 +0600

    Pattern 5

---
+++ Umme_Rubaiyat/Basics of Programming/Pattern/Pattern5.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int stars =1;
+    int spaces =n/2;
+   //write your code here
+   
+   for (int i=1; i <=n; i++)
+   {   
+       for (int j=1; j <=spaces; j++)
+        { 
+             
+                 cout<<"\t";
+              
+                
+        }
+       
+       
+       for (int k=1; k <=stars; k++)
+        {   
+             
+                cout<<"*\t";
+             
+                    
+        }
+        if(i<=n/2)
+        {
+            spaces--;
+            stars +=2;
+
+        }
+        else
+        {
+            spaces++;
+            stars -=2;
+            
+
+        }
+       cout<<endl;
+        
+      
+       
+         
+          
+         
+
+   }
+   
+        
+}
