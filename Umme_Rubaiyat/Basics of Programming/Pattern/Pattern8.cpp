36f16af5d9e906a6bb16bf359b360834d7c892f1
Umme Rubaiyat Chowdhury <ummerubaiyat@gmail.com>
2022-04-26 16:35:43 +0600

    Pattern 8

---
+++ Umme_Rubaiyat/Basics of Programming/Pattern/Pattern8.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int stars =1;
+    int spaces =n ;
+   //write your code here
+   
+   for (int i=1; i <=n; i++)
+   {   
+       for (int j=1; j < spaces; j++)
+        { 
+             
+                 cout<<"\t";
+              
+                
+        }
+       
+       
+       for (int k=1; k <=stars; k++)
+        {   
+             
+                cout<<"*\t";
+             
+                  
+        }
+        if(i<=n )
+        {
+            spaces--;
+             
+
+        }
+         
+       cout<<endl;
+        
+      
+       
+         
+          
+         
+
+   }
+   
+        
+}
